3a1c1b021053a70b99643320244935619eda3076
Robert <rjamesona@icloud.com>
2024-02-25 17:30:01 -0500

    Fix
    Fixed all the things

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-// App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from './styles/Theme.js';
-import Menu from './components/Menu/Menu.js';
-import Home from './views/Home/Home.js';
-import About from './views/About/About.js';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <div>
-          <Menu />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </div>
-      </Router>
-    </ThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
